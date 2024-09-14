32932f98f3e9933ed6154c8527c920fe6274a111
Unshack <marckfriencecoto30@gmail.com>
2024-09-14 15:30:55 +0800

    Finished the hompage initial UI

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -3,27 +3,27 @@ import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
 import ProductItem from "./ProductItem";
 
-const LatestCollection = () => {
+const BestSeller = () => {
   const { products } = useContext(ShopContext);
-  const [latestProducts, setLatestProducts] = useState([]);
+  const [bestSeller, setBestSeller] = useState([]);
 
   useEffect(() => {
-    setLatestProducts(products.slice(0, 10));
+    const bestProduct = products.filter((item) => item.bestseller);
+    setBestSeller(bestProduct.slice(0, 5));
   }, []);
 
   return (
     <div className="my-10">
-      <div className="text-center py-8 text-3xl">
-        <Title text1={"LATEST"} text2={"COLLECTIONS"} />
+      <div className="text-center text-3xl py-8">
+        <Title text1={"BEST"} text2={"SELLERS"} />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-          New style featuring the hottest trends. Be the first to explore and
-          shop before anyone else.
+          These are what everyone’s raving about! Grab yours before they’re
+          gone!
         </p>
       </div>
 
-      {/* Rendering Products */}
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {latestProducts.map((item, index) => (
+        {bestSeller.map((item, index) => (
           <ProductItem
             key={index}
             id={item.id}
@@ -37,4 +37,4 @@ const LatestCollection = () => {
   );
 };
 
-export default LatestCollection;
+export default BestSeller;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src={assets.logo} className="mb-5 w-32" alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            At EdgeWear, we bring you premium-quality, trend-setting t-shirts
+            that merge comfort and style. Browse our collections, stay updated
+            on the latest drops, and enjoy secure, seamless shopping.
+          </p>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+1-234-567-8910</li>
+            <li>contact@edgewear.com</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2024 @edgewear.com - All Right Reserved
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
