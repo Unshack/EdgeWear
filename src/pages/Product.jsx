1fec63251ca7043e5cbbff629eba799b9ee8727b
Unshack <marckfriencecoto30@gmail.com>
2024-09-15 16:03:53 +0800

    Added UI for size and description to individual product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -8,6 +8,7 @@ const Product = () => {
   const { products, currency } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
 
   const fetchProductData = async () => {
     products.map((item) => {
@@ -62,6 +63,53 @@ const Product = () => {
           </p>
           <p className="mt-5 text-gray-500 md:w-4/5">
             {productData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={`border py-2 px-4 bg-gray-100 ${
+                    item === size ? "border-orange-500" : ""
+                  }`}
+                  key={index}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
+        </div>
+      </div>
+
+      {/* ----------- Description & Review Section ---------------- */}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews (450)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            Its minimalist design features a classic crew neck and a relaxed
+            fit, making it versatile for any occasion. Available in a variety of
+            colors, the Classic Cotton T-Shirt is an essential addition to your
+            wardrobe, whether you’re lounging at home, heading out for a casual
+            day, or layering it under your favorite jacket.
+          </p>
+          <p>
+            At EdgeWear, we believe in combining style and sustainability.
+            That’s why our Classic Cotton T-Shirt is made with eco-friendly
+            practices, ensuring a low environmental impact.
           </p>
         </div>
       </div>
