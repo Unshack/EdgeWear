8d45005501a8249729aa5ee697d538ccfb9a4dd9
Unshack <marckfriencecoto30@gmail.com>
2024-10-04 22:22:41 +0800

    product item functionality

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -6,8 +6,12 @@ const ProductItem = ({ id, image, name, price }) => {
   const { currency } = useContext(ShopContext);
 
   return (
-    <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
-      <div className="overflow-hidden">
+    <Link
+      onClick={() => scrollTo(0, 0)}
+      className="text-gray-700 cursor-pointer"
+      to={`/product/${id}`}
+    >
+      <div className=" overflow-hidden">
         <img
           className="hover:scale-110 transition ease-in-out"
           src={image[0]}
@@ -15,7 +19,7 @@ const ProductItem = ({ id, image, name, price }) => {
         />
       </div>
       <p className="pt-3 pb-1 text-sm">{name}</p>
-      <p className="text-sm font-medium">
+      <p className=" text-sm font-medium">
         {currency}
         {price}
       </p>
